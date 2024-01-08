74e470259b7c755799d9aaf94b2b3826cfcc5acb
sergeavila7 <serge_avila7@yahoo.com>
2024-01-08 15:31:35 -0600

    initial config

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import './navbar.scss';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className='wrapper'>
+        <span>Serge Dev</span>
+        <div className='social'></div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
